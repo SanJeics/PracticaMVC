13bd38c39869f9ddd1a07a60f72aed0e07eb15b4
SanJeics <58487003+SanJeics@users.noreply.github.com>
2020-08-30 20:48:55 -0600

    20:38

---
+++ PracticaMVC/Models/AlbumCLS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PracticaMVC.Models
+{
+    public class AlbumCLS
+    {
+        [Display(Name ="Nombre del Album")]
+        string AlbumName { get; set; }
+
+        [Display(Name = "Fecha de Salida")]
+        DateTime DateReleased { get; set; }
+
+        [Display(Name = "Id de Artista")]
+        int ArtistID { get; set; }
+
+        [Display(Name = "Id de Genero")]
+        int GenreID { get; set; }
+
+    }
+}
